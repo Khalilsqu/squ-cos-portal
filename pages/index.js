dea27994c0cd0f2ac09d27d8ad7c7494f40d8cf7
Khalil Al Hooti <hooti@squ.edu.om>
2023-02-15 18:06:16 +0400

    configured hot table

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,42 @@
 import { useRef } from "react";
 import HotTable from "@/components/hotTable";
 
-function Home() {
+function Home(props) {
   const hotRef = useRef(null);
 
   return (
-    <HotTable
-      ref={hotRef}
-      data={[
-        ["", "Kia", "Nissan", "Toyota", "Honda"],
-        ["2008", 10, 11, 12, 13],
-        ["2009", 20, 11, 14, 13],
-        ["2010", 30, 15, 12, 13],
-      ]}
-      licenseKey="non-commercial-and-evaluation"
-    />
+    <div
+      style={{
+        zIndex: 1,
+      }}
+    >
+      <HotTable
+        ref={hotRef}
+        data={[
+          ["", "Kia", "Nissan", "Toyota", "Honda"],
+          ["2008", 10, 11, 12, 13],
+          ["2009", 20, 11, 14, 13],
+          ["2010", 30, 15, 12, 13],
+        ]}
+        licenseKey="non-commercial-and-evaluation"
+        rowHeaders={true}
+        colHeaders={true}
+        width="800px"
+        height="auto"
+        filters={true}
+        dropdownMenu={true}
+        manualColumnResize={true}
+        manualRowResize={true}
+        manualColumnMove={true}
+        manualRowMove={true}
+        contextMenu={true}
+        columnSorting={true}
+        nestedHeaders={[
+          ["", { label: "Car brands", colspan: 4 }],
+          ["Year", "Kia", "Nissan", "Toyota", "Honda"],
+        ]}
+      />
+    </div>
   );
 }
 
