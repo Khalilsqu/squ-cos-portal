6030873b4ad8d0bec68e38484ceead7e4a66f8d9
Khalil Al Hooti <hooti@squ.edu.om>
2023-02-25 13:06:39 +0400

    add nprogress package

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,26 @@ import "@/styles/globals.css";
 import PageLayout from "@/components/layout/pageLayout";
 import { SessionProvider } from "next-auth/react";
 
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import { useEffect } from "react";
+import Router from "next/router";
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
   ...appProps
 }) {
+  useEffect(() => {
+    Router.events.on("routeChangeStart", NProgress.start);
+    Router.events.on("routeChangeComplete", NProgress.done);
+    Router.events.on("routeChangeError", NProgress.done);
+    return () => {
+      Router.events.off("routeChangeStart", NProgress.start);
+      Router.events.off("routeChangeComplete", NProgress.done);
+      Router.events.off("routeChangeError", NProgress.done);
+    };
+  }, []);
   if ([...appProps.router.asPath.split("/")].includes("auth"))
     return (
       <SessionProvider session={session}>
