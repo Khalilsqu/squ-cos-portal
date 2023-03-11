19b11f776c8ede26036429611c06edbd1270da16
Khalil Al Hooti <hooti@squ.edu.om>
2023-03-11 14:38:06 +0400

    add form add image dimns validation

--- components/admin-dashboard/news/addModalNews.js
+++ components/admin-dashboard/news/addModalNews.js
@@ -96,37 +96,42 @@ const ModalData = (props) => {
             return e && e.fileList;
           }}
           rules={[
-            // { required: true, message: "Please input image!" },
+            // { required: true, message: "Please upload an image!" },
             {
               validator: (rule, fileList) => {
                 const fileTypeArray = ["image/png", "image/jpg", "image/jpeg"];
                 return new Promise((resolve, reject) => {
-                  if (fileList === undefined) {
-                    reject("Please upload an image!");
-                  } else if (fileList[0]?.size > 1024 * 1024 * 4.5) {
-                    reject("File size is greater than 4.5MB!");
-                  } else if (
-                    fileList[0] &&
-                    fileTypeArray.includes(fileList[0]?.type) === false
-                  ) {
-                    reject(
-                      "File type is not supported!. Supported types: png, jpg, jpeg"
-                    );
+                  if (fileList !== undefined) {
+                    if (
+                      fileList[0]?.size > 1024 * 1024 * 4.5 &&
+                      fileList.length > 0
+                    ) {
+                      reject("File size is greater than 4.5MB!");
+                    } else if (
+                      fileTypeArray.includes(fileList[0]?.type) === false &&
+                      fileList.length > 0
+                    ) {
+                      reject(
+                        "File type is not supported!. Supported types: png, jpg, jpeg"
+                      );
+                    } else if (fileList.length > 0) {
+                      const img = new Image();
+                      img.src = URL.createObjectURL(fileList[0]?.originFileObj);
+                      img.onload = () => {
+                        const width = img.naturalWidth;
+                        const height = img.naturalHeight;
+                        URL.revokeObjectURL(img.src);
+                        if (width / height < 2) {
+                          reject(
+                            "Image width must be greater than 2 times the height"
+                          );
+                        } else {
+                          resolve("Success!");
+                        }
+                      };
+                    }
                   } else {
-                    const img = new Image();
-                    img.src = URL.createObjectURL(fileList[0]?.originFileObj);
-                    img.onload = () => {
-                      const width = img.naturalWidth;
-                      const height = img.naturalHeight;
-                      URL.revokeObjectURL(img.src);
-                      if (width / height < 2) {
-                        reject(
-                          "Image width must be greater than 2 times the height"
-                        );
-                      } else {
-                        resolve("Success!");
-                      }
-                    };
+                    reject("Please upload an image!");
                   }
                 });
               },
